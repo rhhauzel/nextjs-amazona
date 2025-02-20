12348a10cb30706b939107ffb22be34e24d2b002
Raphael Hauzel <rhhauzel@gmail.com>
2025-02-20 12:08:23 +0530

    09-create-product-detail-page

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Header from '@/components/shared/header'
+import Footer from '@/components/shared/footer'
+
+const RootLayout = ({
+    children,
+}:{
+    children: React.ReactNode
+}) => {
+  return (
+    <div className='flex flex-col min-h-screen'>
+        <Header />
+        <main className='flex-1 flex flex-col p-4'>{children}</main>
+        <Footer />
+    </div>
+  )
+}
+
+export default RootLayout
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,7 @@
-export const APP_NAME=process.env.NEXT_PUBLIC_APP_NAME || 'NextJS'
-export const APP_SLOGAN=process.env.NEXT_PUBLIC_APP_SLOGAN || 'Hmang tlem rawh'
-export const APP_DESCRIPTION=process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Ka in zir ve na'
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NextJS'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Hmang tlem rawh'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Ka in zir ve na'
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
